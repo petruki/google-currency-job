d489b78961cf073ec424c81e2e0122c4e9bb910c
petruki <31597636+petruki@users.noreply.github.com>
2020-10-23 13:18:33 -0700

    Improved input reading

--- src/index.js
+++ src/index.js
@@ -1,59 +1,119 @@
 const { googleCurrencyQuery } = require('./lib/google-currency-query');
 const writeOpenLog = require('./lib/notepad-alert');
 const sendMessage = require('./lib/slack-hook');
-let targetAlarm;
+
+let targetAlarm, maxTarget = 0, minTarget = Number.MAX_VALUE;
+let query, interva, fee;
 
 const notepad_alert = process.env.NOTEPAD_ALERT ? process.env.NOTEPAD_ALERT === 'true' : false;
 const slack_alert = process.env.SLACK_ALERT ? process.env.SLACK_ALERT === 'true' : false;
-
-console.log(`### Google Currency Job Settings ###`);
-console.log(`Notepad alert is: ${notepad_alert}`);
-console.log(`Slack alert is: ${slack_alert}\n`);
-
-function startJob(query, fee, interval) {
-    setInterval(() => {
-        googleCurrencyQuery({ query, fee }).then(results => {
-            console.log(results);
-            if (results.targetAmount <= targetAlarm) {
-                if (notepad_alert) writeOpenLog(results, targetAlarm);
-                if (slack_alert) sendMessage(results, targetAlarm);
-
-                targetAlarm = results.targetAmount - 0.01;
-                console.log(`### Target alarm changed to: ${targetAlarm.toFixed(2)}`);
-            }
-        }).catch(e => {
-            console.log(e);
-        })
-    }, interval * 1000);
-}
 
 const readLine = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-readLine.question('From: ', (from) => {
-    let query = `1 ${from || 'cad'}`;
+const printConsole = (results) => {
+    console.clear();
+    console.log(`### Google Currency Job Settings ###`);
+    console.log(`Notepad alert is: ${notepad_alert ? 'enabled' : 'disabled'}`);
+    console.log(`Slack alert is: ${slack_alert ? 'enabled' : 'disabled'}\n\n`);
 
-    readLine.question('To: ', (to) => {
-        query = `${query} to ${to || 'reais'}`;
+    if (targetAlarm) {
+        console.log(`- Target: ${targetAlarm}`);
+        console.log(`- Max: ${maxTarget}`);
+        console.log(`- Min: ${minTarget}\n\n`);
+        console.log(results);
+    }
+};
 
-        readLine.question('Interval (sec): ', (input) => {
-            let interval = input || 10;
+const job = () => {
+    googleCurrencyQuery({ query, fee }).then(results => {
+            
+        if (results.targetAmount >= maxTarget) maxTarget = results.targetAmount;
+        if (results.targetAmount <= minTarget) minTarget = results.targetAmount;
 
-            readLine.question('Fee (%): ', (input) => {
-                let fee = input || '0.28';
+        printConsole(results);
 
-                readLine.question('Target: ', (target) => {
-                    readLine.close();
-                    targetAlarm = target || 0;
+        if (results.targetAmount <= targetAlarm) {
+            if (notepad_alert) writeOpenLog(results, targetAlarm);
+            if (slack_alert) sendMessage(results, targetAlarm);
 
-                    console.log(`\nQuery: ${query}`);
-                    console.log(`Interval: ${interval}\n`);
-                    startJob(query, `${fee}%`, interval);
-                });
-            });
+            targetAlarm = (results.targetAmount - 0.01).toFixed(2);
+            printConsole(results);
+        }
+    }).catch(e => {
+        console.log(e);
+    });
+};
+
+const scheduleJob = (query, fee, interval) => {
+    job();
+    setInterval(() => job(), interval * 1000);
+};
+
+const askFrom = () => {
+    return new Promise((resolve, reject) => {
+        readLine.question('From: ', (answer) => {
+            query = `1 ${answer || 'cad'}`;
+            resolve();
         });
     });
-});
+};
 
+const askTo = () => {
+    return new Promise((resolve, reject) => {
+        readLine.question('To: ', (answer) => {
+            query = `${query} to ${answer || 'reais'}`;
+            resolve();
+        });
+    });
+};
+
+const askInterval = () => {
+    return new Promise((resolve, reject) => {
+        readLine.question('Interval (sec): ', (answer) => {
+            interval = answer || 10;
+            resolve();
+        });
+    });
+};
+
+const askFee = () => {
+    return new Promise((resolve, reject) => {
+        readLine.question('Fee (%): ', (answer) => {
+            fee = answer || '0.28';
+            resolve();
+        });
+    });
+};
+
+const askTarget = () => {
+    return new Promise((resolve, reject) => {
+        readLine.question('Target: ', (target) => {
+            targetAlarm = target || 0;
+            resolve();
+        });
+    });
+};
+
+const startJob = () => {
+    return new Promise((resolve, reject) => {
+        console.log(`\nQuery: ${query}`);
+        console.log(`Interval: ${interval}\n`);
+        scheduleJob(query, `${fee}%`, interval);
+        resolve();
+    })
+};
+  
+const main = async () => {
+    await askFrom()
+    await askTo()
+    await askInterval()
+    await askFee()
+    await askTarget()
+    await startJob()
+    readLine.close()
+};
+
+main();
